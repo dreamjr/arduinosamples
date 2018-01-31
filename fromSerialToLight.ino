700d7ddcc39f14aac5a9fd2f91fd7113bc733e7f
dreamjr <dreamjr@gmail.com>
2018-01-31 13:00:00 +0900

    0부터 9 사이의 숫자를 입력해주세요. 메시지가 한 번만 출력되도록 처리
    isGuideMessageDisplay 변수를 세팅하여, "0부터 9 사이의 숫자를 입력해주세요" 메시지가 한 번만 출력되도록 함
    시리얼 모니터에 값을 입력하면, 해당 값을 출력해 주고 다시 한 번 "0부터 9 사이의 숫자를 입력해주세요" 메시지를 출력하도록 처리

---
+++ fromSerialToLight.ino
@@ -0,0 +1,47 @@
+const int ledPin = 13;    // 아두이노 보드에 있는 led
+int blinkRate = 0;
+
+bool isGuideMessageDisplay = true;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if(isGuideMessageDisplay) {
+    Serial.println("0부터 9 사이의 숫자를 입력해주세요.");
+    isGuideMessageDisplay = false;
+  }
+  if(Serial.available()) {    
+    //isGuideMessageDisplay = true;
+
+    // #1. serial.read로 읽어옴. 하나 하나씩 읽어와서, 숫자인지 체크
+    /*char ch = Serial.read();
+    if(isDigit(ch)) {
+      blinkRate = ch - '0';
+      blinkRate = blinkRate * 100;
+      Serial.print(ch);
+      Serial.println(" (으)로 값을 설정하셨습니다.");
+      Serial.println("==========================================");
+    }*/
+
+    // #2. Serial.parseInt() 이용
+    int number = Serial.parseInt();
+    blinkRate = number * 100;
+    Serial.print(number);
+    Serial.println(" (으)로 값을 설정하셨습니다.");
+    Serial.println("==========================================");
+    
+    isGuideMessageDisplay = true;    
+  }
+  blink();
+}
+
+void blink() {
+  digitalWrite(ledPin, HIGH);
+  delay(blinkRate);
+  digitalWrite(ledPin, LOW);
+  delay(blinkRate);
+}
+
