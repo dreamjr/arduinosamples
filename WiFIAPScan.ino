adf586b3781a895d9925ab888b198e765aab0fc9
dreamjr <dreamjr@gmail.com>
2019-01-05 12:02:46 +0900

    Add files via upload

---
+++ WiFIAPScan.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  Serial.println("Ready to scan...");
+
+}
+
+void loop() {
+  Serial.println("Scanning start...");
+
+  int n = WiFi.scanNetworks();
+  Serial.println("Scanning completed...");
+
+  if(n == 0) {
+    Serial.println("AP is NOT found...");
+  } else {
+    Serial.print(n);
+    Serial.println(" APs are found...");
+
+    for(int i = 0; i < n; i++) {
+      Serial.print(i + 1);
+      Serial.print(" : ");
+      Serial.print(WiFi.SSID(i));
+
+      Serial.print(" <");
+      Serial.print(WiFi.RSSI(i));
+      Serial.println(" >");
+    }
+  }
+
+  delay(5000);
+
+}
